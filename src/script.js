15730a48f347c1409904978bba87fa8b4ff3c129
vitaliia005 <vitaliia.savchyn.muc@gmail.com>
2022-08-15 18:54:43 +0200

    added icons and unit conversion

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ if (minute < 10) {
 let month = months[currentTime.getMonth()];
 let date = currentTime.getDate();
 let newDate = document.querySelector(".today-date");
-newDate.innerHTML = `${day} ${date} ${month} ${hour}:${minute}`;
+newDate.innerHTML = `${day} <br> ${month}, ${date} ${hour}:${minute}`;
 
 function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -45,13 +45,10 @@ function searchCity(city) {
   axios.get(apiUrl).then(showTemperature);
 }
 function showTemperature(response) {
-  if (response.data.name === "Altstadt") {
-    document.querySelector(".city").innerHTML = "Munich";
-  }
   document.querySelector(".city").innerHTML = response.data.name;
-  document.querySelector(".today-temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  celsiusTemperature = response.data.main.temp;
+  document.querySelector(".today-temp").innerHTML =
+    Math.round(celsiusTemperature);
   document.querySelector(".description").innerHTML =
     response.data.weather[0].description;
 
@@ -60,6 +57,18 @@ function showTemperature(response) {
   document.querySelector(".wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  document
+    .querySelector(".icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector(".icon")
+    .setAttribute(
+      "alt",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+    );
 }
 
 function handleSubmitButton(event) {
@@ -74,14 +83,40 @@ function showPosition(position) {
   axios.get(apiUrl).then(showTemperature);
 }
 
+function showFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+
+  document.querySelector(".today-temp").innerHTML = Math.round(
+    fahrenheitTemperature
+  );
+  console.log(document.querySelector(".today-temp").innerHTML);
+  console.log(fahrenheitTemperature);
+}
+
+function showCelsius(event) {
+  event.preventDefault();
+  document.querySelector(".today-temp").innerHTML =
+    Math.round(celsiusTemperature);
+}
+
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
 }
+
+let celsiusTemperature = null;
+
 let searchForm = document.querySelector(".city-form");
 searchForm.addEventListener("submit", handleSubmitButton);
 
 let currentCityButton = document.querySelector(".current-city");
 currentCityButton.addEventListener("click", getCurrentLocation);
+
+let fahrenheitLink = document.querySelector(".fahrenheit");
+fahrenheitLink.addEventListener("click", showFahrenheit);
+
+let celsiusLink = document.querySelector(".celsius");
+fahrenheitLink.addEventListener("click", showCelsius);
 
 searchCity("Munich");
